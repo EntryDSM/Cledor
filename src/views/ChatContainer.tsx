a4ad06a502124c8222a2a4b1ee7ff98f78f820f6
JoonmoYeon <yunedoopea610@gmail.com>
2019-03-31 11:54:38 +0900

    Add chat container style

--- src/views/ChatContainer.tsx
+++ src/views/ChatContainer.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import InfoBar from './InfoBar';
 import ChatBoard from './ChatBoard';
 import ChatBar from './ChatBar';
+import { ChatContainerCover } from './styled-components/ChatContainer';
 
 export interface ChatContainerProps {}
 
@@ -20,13 +21,15 @@ export default class ChatContainer extends React.Component<
   // tslint:disable-next-line: variable-name
   send = (_content: string, _imageData: File) => null;
 
+  close = () => null;
+
   public render() {
     return (
-      <div>
-        <InfoBar onlines={0} />
+      <ChatContainerCover>
+        <InfoBar onlines={0} onClose={this.close} />
         <ChatBoard />
         <ChatBar send={this.send} />
-      </div>
+      </ChatContainerCover>
     );
   }
 }
