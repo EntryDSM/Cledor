1913c6ee007fac08461d3688d2ffdd970dbf967a
JoonmoYeon <yunedoopea610@gmail.com>
2019-03-31 12:02:44 +0900

    Add info bar style

--- src/views/InfoBar.tsx
+++ src/views/InfoBar.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
+import {
+  InfoBarCover,
+  InfoBarText,
+  Onlines,
+  CloseButton,
+} from './styled-components/InfoBar';
 
 export interface InfoBarProps {
   onlines: number;
+  onClose: () => void;
 }
 
 // tslint:disable-next-line: variable-name
-export default function infoBar(_props: InfoBarProps) {
-  return <div />;
+export default function infoBar({ onlines, onClose }: InfoBarProps) {
+  return (
+    <InfoBarCover>
+      <CloseButton onClick={onClose}>×</CloseButton>
+      <InfoBarText>현재 관리자</InfoBarText>
+      <Onlines>{onlines}명</Onlines>
+    </InfoBarCover>
+  );
 }
