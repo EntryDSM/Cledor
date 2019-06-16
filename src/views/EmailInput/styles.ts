e2bb9f5400e6f9889a9aeef466e2cfd4827b2362
Joonmo-dev <yunedoopea610@gmail.com>
2019-06-16 16:09:44 +0900

    Add guding message animation

--- src/views/EmailInput/styles.ts
+++ src/views/EmailInput/styles.ts
@@ -1,17 +1,41 @@
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 import Theme from '../../Theme';
 
 export const EmailInputForm = styled.form`
   padding: 10px;
 `;
 
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+
+  50% {
+    transform: translateY(0);
+  }
+
+  60% {
+    opacity: 0;
+  }
+
+  75% {
+    transform: translateY(-20px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
 export const GuidingMessage = styled.div`
-  margin: 0 70px 10px 10px;
+  margin: 0 70px 20px 10px;
   border: 1px ${Theme.MAIN_COLOR4} solid;
   padding: 5px 10px;
   font-size: 12px;
   border-radius: ${Theme.APEX_RADIUS};
   border-bottom-left-radius: 0;
+  animation: ${fadeIn} 1.5s ease-in-out;
 `;
 
 export const EmailInput = styled.input<{ hasError: boolean }>`
